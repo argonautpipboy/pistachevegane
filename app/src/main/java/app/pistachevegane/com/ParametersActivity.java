b995aa4b9802562be81294f10e1702e1176761de
GRAVELLE Boris <gravelleboris@gmail.com>
2017-01-22 13:26:36 +0100

    Initialisation of parameter view

--- app/src/main/java/app/pistachevegane/com/ParametersActivity.java
+++ app/src/main/java/app/pistachevegane/com/ParametersActivity.java
@@ -7,8 +7,20 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import app.pistachevegane.com.components.ExpandableListAdapter;
 
 public class ParametersActivity extends CommonActivity {
+
+    private ExpandableListAdapter listAdapter;
+    private ExpandableListView expListView;
+    private List<String> listDataHeader;
+    private HashMap<String, List<String>> listDataChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +39,14 @@ public class ParametersActivity extends CommonActivity {
         navigationView = (NavigationView) findViewById(R.id.nav_view_parameters);
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.getMenu().getItem(5).setChecked(true);
+
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.list_params);
+        // prepare the datas
+        prepareDatas();
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
     }
 
     @Override
@@ -43,5 +63,28 @@ public class ParametersActivity extends CommonActivity {
             drawer.closeDrawer(GravityCompat.START);
         }
         return super.onNavigationItemSelected(item);
+    }
+
+    private void prepareDatas(){
+        listDataHeader = new ArrayList<String>();
+
+        listDataHeader.add(getString(R.string.parameters_title_whoweare));
+        listDataHeader.add(getString(R.string.parameters_title_contactus));
+        listDataHeader.add(getString(R.string.parameters_title_licenceandthanks));
+
+        List<String> listDataWho = new ArrayList<String>();
+        listDataWho.add(getString(R.string.parameters_label_whoweare));
+
+        List<String> listDataContact = new ArrayList<String>();
+        listDataContact.add(getString(R.string.parameters_label_contactus));
+
+        List<String> listDataLicence = new ArrayList<String>();
+        listDataLicence.add(getString(R.string.parameters_label_licenceandthanks1));
+        listDataLicence.add(getString(R.string.parameters_label_licenceandthanks2));
+
+        listDataChild = new HashMap<String, List<String>>();
+        listDataChild.put(listDataHeader.get(0), listDataWho);
+        listDataChild.put(listDataHeader.get(1), listDataContact);
+        listDataChild.put(listDataHeader.get(2), listDataLicence);
     }
 }
